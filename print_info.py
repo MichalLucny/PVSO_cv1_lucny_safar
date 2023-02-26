3cd4634e1ae69e29965fc446b391d9848dc98b74
Patrik <xsafarp@stuba.sk>
2023-02-26 19:42:51 +0100

    print info

---
+++ print_info.py
@@ -0,0 +1,8 @@
+import numpy
+from numpy import *
+import cv2
+
+img = cv2.imread('out.jpg')
+print("Datovy typ:", img.dtype)
+print("\nRozmer:", img.shape[0], "x", img.shape[1])
+print("\nVelkost:", img.size, "px")
